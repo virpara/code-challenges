9f6663c48ccce734420f9f9082d58f588a5fa8aa
virpara <virpararahul@gmail.com>
2018-01-01 17:52:35 +0530

    Got 10 points. Unoptimized version.

---
+++ src/com/codechef/ltime55/StrangeFunc.java
@@ -0,0 +1,41 @@
+package com.codechef.ltime55;
+
+import java.util.Scanner;
+
+public class StrangeFunc {
+
+	// TODO: gained 10 points. optimize the approach
+	public static void main(String[] args) throws java.lang.Exception {
+		
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		for (int i = 0; i < t; i++) {
+			int a = s.nextInt();
+			int n = s.nextInt();
+			
+			long ans = f((long) Math.pow(a, n));
+			
+			System.out.println(ans);
+		}
+	}
+	
+	static long f(long a) {
+		long s = sum(a); 
+	    if (s < 10) return s;
+	    else {
+	    	return f(s);
+	    }
+	}
+
+	static long sum(long a) {
+	    long sum = 0l;
+	    
+	    while (a > 0) {
+	    	//System.out.println(a%10);
+	        sum += a%10;
+	        a = a / 10;
+	    }
+	    
+	    return sum;
+	}
+}

aee9d1b9aaf2a7b562916e6759abe4613ad25946
virpara <virpararahul@gmail.com>
2018-01-13 03:14:32 +0530

    added random test case generation

--- src/com/codechef/jan18/KConcatenation.java
+++ src/com/codechef/jan18/KConcatenation.java
@@ -1,78 +1,120 @@
 package com.codechef.jan18;
 
-import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class KConcatenation {
+	private static File f = new File("input.txt");
 
-  public static void main(String[] args) {
-    generateTests();
-    
-    Scanner s = new Scanner(System.in);
-    int t = s.nextInt();
-    for (int i = 0; i < t; i++) {
-      int n = s.nextInt();
-      int k = s.nextInt();
+	public static void main(String[] args) throws FileNotFoundException {
+//		generateTests();
 
-      int[] num = new int[n];
-      for (int j = 0; j < n; j++) {
-        num[j] = s.nextInt();
-      }
+		Scanner s = new Scanner(System.in);
+//		Scanner s = new Scanner(f);
+		int t = s.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = s.nextInt();
+			int k = s.nextInt();
 
-      int sum = 0;
-      int maxSum = 0;
-      boolean isNeg = false;
+			int[] num = new int[n];
+			for (int j = 0; j < n; j++) {
+				num[j] = s.nextInt();
+			}
 
-      int sumStart = 0;
-      int lastNegIdx = 0;
-        
-      for (int j = 0; j < n; j++) {
-        if (num[j] > 0) {
-          sum += num[j];
-        } else if (num[j] < 0) {
-          maxSum = sum > maxSum ? sum : maxSum;
+			long sum = 0;
+			long maxSum = 0;
+			boolean isNeg = false;
 
-          // store sum before first negative num
-          if (!isNeg) {
-            sumStart = sum;
-            lastNegIdx = j;
-          }
+			long sumStart = 0;
+			int lastNegIdx = 0;
 
-          sum = 0;
-          isNeg = true;
-        }
-      }
-          
-      // if last digit is positive
-      maxSum = sum > maxSum ? sum : maxSum;
+			for (int j = 0; j < n; j++) {
+				if (num[j] > 0) {
+					sum += num[j];
+				} else if (num[j] < 0) {
+					maxSum = sum > maxSum ? sum : maxSum;
 
-      if (lastNegIdx > 0) {
-        for (int j = lastNegIdx + 1; j < n; j++) {
-          sumStart += num[j];
-        }
+					// store sum before first negative num
+					if (!isNeg) {
+						sumStart = sum;
+						lastNegIdx = j;
+					}
 
-        maxSum = sumStart > maxSum ? sumStart : maxSum;
-      }
+					sum = 0;
+					isNeg = true;
+				}
+			}
 
+			// if last digit is positive
+			maxSum = sum > maxSum ? sum : maxSum;
 
+			if (lastNegIdx > 0) {
+				for (int j = lastNegIdx + 1; j < n; j++) {
+					sumStart += num[j];
+				}
 
-      if (!isNeg) {
-        maxSum = maxSum > 0 ? maxSum * k : sum * k;
-      } else {
-        maxSum = maxSum > 0 ? maxSum : sum;
-      }
+				maxSum = sumStart > maxSum ? sumStart : maxSum;
+			}
 
+			if (!isNeg) {
+				maxSum = maxSum > 0 ? maxSum * k : sum * k;
+			} else {
+				maxSum = maxSum > 0 ? maxSum : sum;
+			}
 
-      System.out.println(maxSum);
-        
-    }
-    
-  }
+			System.out.println(maxSum);
 
-  private static void generateTests() {
-    InputStream is = System.in;
-    
-    // TODO
-  }
+		}
+
+	}
+
+	private static void generateTests() {
+		// InputStream is = System.in;
+		Random rn = new Random();
+		
+		BufferedWriter br = null;
+		try {
+			br = new BufferedWriter(new FileWriter(f));
+			
+			int t = rn.nextInt(9) + 1 ;
+			br.write(t + "\n");
+			for (int j = 0; j < t; j++) {
+				int k = rn.nextInt(1000000);
+				int n = rn.nextInt(1000000);
+				br.write(n + " " + k + "\n");
+//				System.out.println(n + " " + k);
+
+				int a;
+				for (int i = 0; i < n; i++) {
+					a = rn.nextInt(10000000) * (Math.random() > 0.5 ? 1 : 1);
+//					System.out.print(a + " ");
+					br.write(a + " ");
+				}
+				br.write("\n");
+//				System.out.println();
+			}
+			
+			br.flush();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
 
 }

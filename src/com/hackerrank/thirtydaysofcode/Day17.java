78e55118466f373ae98feda45b9065f9c8c95143
virpara <virpararahul@gmail.com>
2018-01-21 03:46:18 +0530

    fixed typo to get AC

--- src/com/hackerrank/thirtydaysofcode/Day17.java
+++ src/com/hackerrank/thirtydaysofcode/Day17.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Calculator {
     
     public int power (int n, int p) throws Exception {
-        if (n < 0 || p < 0) throw new Exception("n and p shoud be non-negative");
+        if (n < 0 || p < 0) throw new Exception("n and p should be non-negative");
         
         if (p == 0) return 1;
         if (n == 0) return 0;

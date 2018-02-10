8cf22ff3fe73fc2ba05648033b494162711c8147
virpara <virpararahul@gmail.com>
2018-02-10 18:01:50 +0530

    traditional tower of hanoi with three pegs

---
+++ src/com/prob/TowerOfHanoi.java
@@ -0,0 +1,29 @@
+package com.prob;
+
+
+public class TowerOfHanoi {
+	
+	public static void main(String[] args) {
+		int n = 10;
+		char a = 'A', b = 'B', c = 'C';
+		
+		solve(n, a, b, c);
+		
+	}
+	
+	private static void solve(int n, char a, char b, char c) {
+		if (n == 1) {
+			printStep(n, a, c);
+		} else {
+			solve(n-1, a, c, b);
+			printStep(n, a, c);
+			solve(n-1, b, a, c);
+		}
+		
+	}
+
+	public static void printStep(int n, char x, char y) {
+		System.out.println("move " + n + " from " + x + " to " + y);
+	}
+	
+}

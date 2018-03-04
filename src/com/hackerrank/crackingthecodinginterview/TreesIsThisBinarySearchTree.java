e36972865002df4692af08baf135a26c113e58d8
virpara <virpararahul@gmail.com>
2018-03-04 14:37:31 +0530

    fails if right most in left subtree is greater than root and vice-versa

--- src/com/hackerrank/crackingthecodinginterview/TreesIsThisBinarySearchTree.java
+++ src/com/hackerrank/crackingthecodinginterview/TreesIsThisBinarySearchTree.java
@@ -16,32 +16,111 @@ public class TreesIsThisBinarySearchTree {
 	}
 
 	boolean checkBST(Node root) {
-		if (root == null)
-			return false;
-		
-		Node left = root.left;
-		Node right = root.right;
-		
-		boolean isBst = false;
-		
-		if (left == null && right == null) {
-			return true;
-		}
-
-		if (left == null || right == null) {
-			return false;
-		}
-		
-		if (left.data > root.data || right.data < root.data) {
-			return false;
-		} else {
-			isBst = checkBST(left) && checkBST(right);
-		}
-		
-		
-		return isBst;
+		if (root != null) {
+            Node left = root.left;
+            Node right = root.right;
+            
+            System.out.println(" root=" + root + ", left=" + left + ", right=" + right);
+            
+            boolean l = true, r = true;
+            if ( left != null) {
+            	// to check each element in the left tree is less than or equal to root value.
+            	
+                if (left.data <= root.data) {
+                    //System.out.print(", left.data=" + left.data);
+                    l = isBSTLeft(left, root);
+                } else 
+                    l = false;
+                
+            }
+            
+            if (right != null) {
+            	// each element in the right tree is greater than root value
+            	
+            	if (right.data > root.data) {
+                    //System.out.print(", right.data=" + right.data);
+                    r = isBSTRight(right, root);
+                } else
+                    r = false;
+            }
+            
+            return (l && r);
+            
+        } else return true;
 	}
 	
+	boolean isBSTLeft(Node curr, Node prev) {
+		if (curr != null) {
+            Node left = curr.left;
+            Node right = curr.right;
+            
+            boolean l = true, r = true;
+            if ( left != null) {
+            	// to check each element in the left tree is less than or equal to root value.
+            	
+                if (left.data <= curr.data) {
+                    //System.out.print(", left.data=" + left.data);
+                    l = isBSTLeft(left, prev);
+                } else {
+                	l = false;
+                }
+                
+            }
+            
+            if (right != null) {
+            	// each element in the right tree is greater than root value
+            	
+            	if (right.data > curr.data) {
+                    //System.out.print(", right.data=" + right.data);
+                    r = isBSTRight(right, prev);
+                } else
+                    r = false;
+            } else if (curr.data > prev.data) {
+            	r = false;
+            }
+            
+            return (l && r);
+            
+        } else return true;
+		
+	}
+	
+	boolean isBSTRight(Node curr, Node prev) {
+		if (curr != null) {
+            Node left = curr.left;
+            Node right = curr.right;
+            
+            boolean l = true, r = true;
+            if ( left != null) {
+            	// to check each element in the left tree is less than or equal to root value.
+            	
+                if (left.data <= curr.data) {
+                    //System.out.print(", left.data=" + left.data);
+                    l = isBSTLeft(left, prev);
+                } else 
+                    l = false;
+                
+            } else if (curr.data <= prev.data) {
+            	l = false;
+            }
+            
+            if (right != null) {
+            	// each element in the right tree is greater than root value
+            	
+            	if (right.data > curr.data) {
+                    //System.out.print(", right.data=" + right.data);
+                    r = isBSTRight(right, prev);
+                } else
+                    r = false;
+            }
+            
+            return (l && r);
+            
+        } else return true;
+		
+	}
+	
+
 	public static void main() {
 		
 	}

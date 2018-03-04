f4e83f478af52bc099ff68837576ea5a89afea78
virpara <virpararahul@gmail.com>
2018-03-04 14:38:36 +0530

    word anagram incomplete

--- src/com/oracle/WordAnagrams.java
+++ src/com/oracle/WordAnagrams.java
@@ -1,4 +1,4 @@
-package com.oracle;
+package com.oracle;	
 
 public class WordAnagrams {
 	// TODO
@@ -13,52 +13,30 @@ public class WordAnagrams {
 		String word = "abcde";
 		
 		char[] chs = word.toCharArray();
-		int j = 0;
 		
-		for (int i = chs.length-1; i >= 0; i--) {
-			//anagram(chs, i); // should print all anagrams starting with a, c and b
-			System.out.println(new String(chs));
-			swap(chs, 0, i);
+		prepare(chs, 1);
+		
+	}
+	public static void prepare(char[] w, int i) {
+		System.out.println(String.valueOf(w));
+		if (i <= w.length-1) {
+			swap(w, 0, i);
+			prepare(w, i+1);
 		}
 		
 		
-		
-		
-//		int possibleAnagrams = fact(chs.length);
-//		
-		//doesn't work for string length > 3 :(
-//		int x = 2;
-//		int y = 1;
-//		for (int i = 0; i < possibleAnagrams; i++) {
-//			
-//			System.out.print("i=" + i + " x=" + x + " y=" + y + " ");
-//			swap(chs, x, y); 
-//			System.out.println(new String(chs));
-//			
-//			x++;
-//			y++;
-//			
-//			if (y == chs.length) y = 0;
-//			
-//			if (x == chs.length) x = 0;
-//		}
-		
-		
+	}
+	public static void anagram(char[] w, int a, int i, int j) {
 		
 	}
 	
 	public static void swap(char[] word, int i, int j) {
-		if (i > word.length - 1 || j > word.length - 1) throw new IllegalArgumentException();
+//		if (i > word.length - 1 || j > word.length - 1) throw new IllegalArgumentException();
 		
 		char c = word[i];
 		word[i] = word[j];
 		word[j] = c;
 	}
 	
-	public static int fact(int num) {
-		if(num == 1) return 1;
-		
-		return num * fact(num-1);
-	}
 
 }

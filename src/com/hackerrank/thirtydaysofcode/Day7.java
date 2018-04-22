86d1752aaef9bc228b7a77a94a6eeebe4c11a752
virpara <virpararahul@gmail.com>
2018-04-23 01:09:21 +0530

    changed to utf-8 encoding

--- src/com/hackerrank/thirtydaysofcode/Day7.java
+++ src/com/hackerrank/thirtydaysofcode/Day7.java
@@ -2,7 +2,6 @@ package com.hackerrank.thirtydaysofcode;
 
 import java.util.Scanner;
 
-
 public class Day7 {
 
     public static void main(String[] args) {
@@ -13,9 +12,9 @@ public class Day7 {
             arr[i] = in.nextInt();
         }
         in.close();
-        
+
         for (int i = arr.length - 1; i >= 0; i--) {
-        	System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 }

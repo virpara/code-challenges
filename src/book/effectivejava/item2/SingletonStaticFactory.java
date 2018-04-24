d08e28237733d48ef94345ef4110597a33f58d37
virpara <virpararahul@gmail.com>
2018-04-24 18:17:12 +0530

    Added serialization to maintain true singleton

--- src/book/effectivejava/item2/SingletonStaticFactory.java
+++ src/book/effectivejava/item2/SingletonStaticFactory.java
@@ -1,6 +1,9 @@
 package book.effectivejava.item2;
 
-public class SingletonStaticFactory {
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public class SingletonStaticFactory implements Serializable{
 
     private static final SingletonStaticFactory INSTANCE = new SingletonStaticFactory();
 
@@ -9,6 +12,17 @@ public class SingletonStaticFactory {
     }
 
     public static SingletonStaticFactory getInstance() {
+        return INSTANCE;
+    }
+
+    // This applies to both the singleton approaches
+    // To make a singleton class serializable (Chapter 10), it is not sufficient merely to add
+    // implements Serializable to its declaration. To maintain the singleton guarantee, you must
+    // also provide a readResolve method (Item 57). Otherwise, each deserialization of a serialized
+    // instance will result in the creation of a new instance, leading, in the case of our example, to
+    // spurious Elvis sightings. To prevent this, add the following readResolve method to the
+    // Elvis class:
+    private Object readResolve() throws ObjectStreamException {
         return INSTANCE;
     }
 }

4b379114dae229b8506bd627bad291966ec61d66
virpara <virpararahul@gmail.com>
2018-10-15 00:50:26 +0530

    Added method to get string from scanner

--- src/com/util/Scan.java
+++ src/com/util/Scan.java
@@ -3,31 +3,35 @@ package com.util;
 import java.util.Scanner;
 
 public class Scan {
-	private static final Scanner s = new Scanner(System.in);
-	
-	public static void close() {
-		s.close();
-	}
-	
-	public static int[] nia(int n) {
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = s.nextInt();
-		}
-		
-		return a;
-	}
+    private static final Scanner s = new Scanner(System.in);
 
-	public static Integer[] nIa(int n) {
-		Integer[] a = new Integer[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = s.nextInt();
-		}
+    public static void close() {
+        s.close();
+    }
 
-		return a;
-	}
-	
-	public static int ni() {
-		return s.nextInt();
-	}
+    public static int[] nia(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+
+        return a;
+    }
+
+    public static Integer[] nIa(int n) {
+        Integer[] a = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+
+        return a;
+    }
+
+    public static int ni() {
+        return s.nextInt();
+    }
+
+    public static String ns() {
+        return s.next();
+    }
 }

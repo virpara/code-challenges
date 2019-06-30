b4e7d7d80391c4703222255fe1012bfc153eb273
virpara <virpararahul@gmail.com>
2019-06-30 23:03:25 +0530

    generate possible words

--- src/com/oracle/WordAnagrams.java
+++ src/com/oracle/WordAnagrams.java
@@ -7,44 +7,16 @@ import com.util.Scan;
  */
 
 public class WordAnagrams {
-    private static boolean[] isChsIUsed;
+
     private static int counter = 0;
-
-    private static void word(int idx, char[] chs, char[] word) {
-        // we have shuffled all the characters, so just print them now
-        if (idx == chs.length) {
-            System.out.println(new String(word));
-            return;
-        }
-
-        for (int i = 0; i < chs.length; i++) {
-            counter++;
-            if (isChsIUsed[i]) continue;
-
-            word[idx] = chs[i];
-            isChsIUsed[i] = true;
-
-            word(idx + 1, chs, word);
-
-            isChsIUsed[i] = false;
-        }
-    }
 
     public static void main(String[] args) {
         System.out.print("Enter a word: ");
         String word = Scan.ns();
         char[] chs = word.toCharArray();
 
-//        // tmp var to hold an anagram
-//        char[] tmp = new char[word.length()];
-//        isChsIUsed = new boolean[chs.length];
-//
-//        word(0, chs, tmp);
-//        System.out.println("counter: " + counter);
-
-        // better approach
         anagram(chs, 0, chs.length - 1);
-//        System.out.println("counter: " + counter);
+        System.out.println("counter: " + counter);
     }
 
     private static void anagram(char[] word, int l, int r) {

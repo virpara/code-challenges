b6c0733900f0b2f4168c68c04bb8cc13101ab43a
virpara <virpararahul@gmail.com>
2019-07-19 00:09:16 +0530

    moved to package misc

---
+++ src/com/misc/Palindrome.java
@@ -0,0 +1,22 @@
+package com.misc;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String s = "abcbaa";
+		char[] chs = s.toCharArray();
+		System.out.println(isPalindrome(chs, 0, chs.length-1));
+
+	}
+
+	private static boolean isPalindrome(char[] s, int i, int j) {
+		if (j - i < 1) {
+			return true;
+		} else if (s[i] == s[j]) {
+			return isPalindrome(s, i+1, j-1);
+		} else {
+			return false;
+		}
+	}
+
+}

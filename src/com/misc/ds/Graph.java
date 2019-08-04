147ac6df8917f57d2b4b73e9b13a68ce3c311c34
virpara <virpararahul@gmail.com>
2019-08-04 22:19:52 +0530

    Graph depth first search traversal

--- src/com/misc/ds/Graph.java
+++ src/com/misc/ds/Graph.java
@@ -11,16 +11,55 @@ public class Graph {
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph(6, false);
+        Graph g = new Graph(13, false);
 
-        g.addEdge(2, 4, 1);
-        g.addEdge(2, 5, 1);
+        g.addEdge(0, 1, 1);
+        g.addEdge(1, 2, 12);
+        g.addEdge(1, 3, 10);
+        g.addEdge(2, 4, 11);
+        g.addEdge(2, 5, 13);
+        g.addEdge(3, 5, 12);
+        g.addEdge(3, 7, 13);
+        g.addEdge(4, 6, 9);
+        g.addEdge(5, 6, 7);
+        g.addEdge(6, 7, 15);
+        g.addEdge(8, 9, 12);
+        g.addEdge(8, 10, 10);
+        g.addEdge(9, 10, 10);
+        g.addEdge(10, 11, 9);
+        g.addEdge(11, 12, 10);
 
         g.print();
-
+        
+        g.dfs_traversal();
     }
 
-    private void print() {
+    public void dfs_traversal() {
+        if (edges.length == 0) return;
+
+        boolean[] isVisited = new boolean[edges.length];
+
+        for (int i = 0; i < edges.length; i++)
+            dfs(i, isVisited);
+    }
+
+    private void dfs(int u, boolean[] isVisited) {
+        if (isVisited[u]) {
+            return;
+        }
+
+        System.out.print(u + " ");
+
+        // otherwise mark u visited and visit all the neighbours of u
+        isVisited[u] = true;
+        EdgeNode e = edges[u];
+        while (e != null) {
+            dfs(e.getV(), isVisited);
+            e = e.getNext();
+        }
+    }
+
+    public void print() {
         for (int i = 0; i < edges.length; i++) {
             System.out.print(i + " --> ");
 

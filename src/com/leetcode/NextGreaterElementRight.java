fa6cc548e22154652a6c2d0abb7445050602e7fa
Rahul Virpara <virpararahul@gmail.com>
2024-06-02 23:47:34 +0530

    Solved next greater element right with left to right iteration

--- src/com/leetcode/NextGreaterElementRight.java
+++ src/com/leetcode/NextGreaterElementRight.java
@@ -12,6 +12,33 @@ public class NextGreaterElementRight {
         System.out.println("ans=" + Arrays.toString(ans));
     }
 
+    // loop from right to left and use stack with operation pop all less than current, answer, push
+//    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//        Deque<Integer> stack = new ArrayDeque<>();
+//
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        int[] ans = new int[nums1.length];
+//
+//        for (int i = nums2.length - 1; i >= 0; i--) {
+//            while (!stack.isEmpty() && nums2[i] > stack.peekFirst()) {
+//                stack.removeFirst();
+//            }
+//
+//            if (stack.isEmpty()) map.put(nums2[i], -1);
+//            else map.put(nums2[i], stack.peekFirst());
+//
+//            stack.addFirst(nums2[i]);
+//        }
+//
+//        for (int i = 0; i < nums1.length; i++) {
+//            ans[i] = map.get(nums1[i]);
+//        }
+//
+//        return ans;
+//    }
+
+    // approach 2: left to right iteration
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Deque<Integer> stack = new ArrayDeque<>();
 
@@ -19,15 +46,19 @@ public class NextGreaterElementRight {
 
         int[] ans = new int[nums1.length];
 
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums2[i] > stack.peekFirst()) {
+        for (int i = 0; i < nums2.length; i++) {
+
+            while (!stack.isEmpty() && nums2[i] > nums2[stack.peekFirst()]) {
+                map.put(nums2[stack.peekFirst()], nums2[i]);
                 stack.removeFirst();
             }
 
-            if (stack.isEmpty()) map.put(nums2[i], -1);
-            else map.put(nums2[i], stack.peekFirst());
+            stack.addFirst(i);
+        }
 
-            stack.addFirst(nums2[i]);
+        while (!stack.isEmpty()) {
+            map.put(nums2[stack.peekFirst()], -1);
+            stack.removeFirst();
         }
 
         for (int i = 0; i < nums1.length; i++) {

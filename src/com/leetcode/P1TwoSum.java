0a2c75b4e6aa5144e3f557a6bfaec0912b90d476
Rahul Virpara <virpararahul@gmail.com>
2025-01-15 22:25:32 +0530

    Solved P1

--- src/com/leetcode/P1TwoSum.java
+++ src/com/leetcode/P1TwoSum.java
@@ -2,13 +2,13 @@ package com.leetcode;
 
 import java.util.Arrays;
 
-public class TwoSum {
+public class P1TwoSum {
 
     public static void main(String[] args) {
 
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int[] result = new TwoSum().twoSum(nums, target);
+        int[] result = new P1TwoSum().twoSum(nums, target);
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
